ed493ba87ad5adb939b3101c2c4430e8229603ab
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-13 12:30:24 -0500

    Add files via upload
    These are the files that pull up the dog photos.

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import requests
+
+from dog_breeds import prettify_dog_breed
 
 app = Flask("app")
 
-@app.route("/")
-def hello_world():
-  return render_template("dogs.html")
+def check_breed(breed):
+  return "/".join(breed.split("-"))
 
-app.run(host='0.0.0.0', port=5000)
+@app.route("/", methods=["GET","POST"])
+def dog_image_gallery():
+  errors = []
+  if request.method == "POST":
+    breed = request.form.get("breed")
+    number = request.form.get("number")
+    if not breed:
+      errors.append("Oops! Please choose a breed.")
+    if not number:
+      errors.append("Oops! Please choose a number.")
+    if breed and number:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
+      data = response.json()
+      dog_images = data["message"]
+      return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
+  return render_template("dogs.html", images=[], breed="", errors=errors)
+
+
+app.debug = True
+app.run(host='0.0.0.0', port=8080)
