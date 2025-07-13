84057fd35620e2bac9c730c79a2dfb41b3f9323f
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-13 06:17:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import requests
 
 app = Flask("app")
 
-@app.route("/")
-def hello_world():
+#function adds a dash in between breed names with multiple words like miniature poodle
+def check_breed(breed):
+  return "/".join(breed.split("-"))
+
+@app.route("/", methods=["GET", "POST"])
+def dog_image_gallery():
+  errors=[]
+  if request.method == "POST":
+    breed=request.form.get("breed")
   return render_template("dogs.html")
 
+app.debug = True
+
+app.debug = True
 app.run(host='0.0.0.0', port=5000)
