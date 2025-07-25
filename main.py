e24b5a24827a551865be981a4f06177376a5c291
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-25 08:27:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import requests
 
 from dog_breeds import prettify_dog_breed
 
+
+#main.py, dog_breeds.py are files  used to create a web app that allows users to select a dog breed and a number of images to display.
+#In static folder, there is dogs.css used to style the web app.
+#In templates folder, there is dogs.html used to create the web app.
+
+
 app = Flask("app")
 
 def check_breed(breed):
@@ -26,6 +32,13 @@ def dog_image_gallery():
       return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
   return render_template("dogs.html", images=[], breed="", errors=errors)
 
+@app.route("/random", methods=["POST"])
+def get_random_dog():
+  response = requests.get("https://dog.ceo/api/breeds/image/random")
+  data = response.json()
+  dog_image = data["message"]
+  return render_template("dogs.html", images=[dog_image], breed="Random", errors=[])
+  
 
 app.debug = True
 app.run(host='0.0.0.0', port=8080)
